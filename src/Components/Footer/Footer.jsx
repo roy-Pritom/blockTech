d299344d5297fed0a5b1c3179449b608108e8539
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-08-13 00:22:45 +0600

    code done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-
+import logo from '../../assets/dynaqube1.png';
 const Footer = () => {
     return (
         <div className=" p-10   text-white mt-10">
             <footer className="footer md:flex md:justify-between p-7 md:p-10  text-white">
                 <div>
-                    <div className="flex items-center justify-center">
-                        <img src="https://s3-alpha-sig.figma.com/img/629f/f9bc/1671f45dd70fcc98ccd63fd9b593b28e?Expires=1692576000&Signature=DpbXVBjhLgSPmCL9UjKp5Ni6Y5FASMGEdQcFQRLHLk2LJ5uGOQZMvrDZk67ZtrqzMkxixUQRH6agiQntzq4UkkXjwlMaNNHGUniVRTfQBkEZpqKyqGPQHVBd93pY2D~ySEmNDPrRXU0QT5XxFvI2vTAve~A19g46IKNFalkUigvSz3lSeucSJA2LXJaG6C1ZcneIqLxcSzarNTlQy3NwWkT8q6tlc4L2RXrZMit3LnbZyrakZc0LwCzWSRNWs93gFCjYLiTNUYQwCtQL7hE0sNGiNXjbMO0PPkQ2zunHiysD87Mr7b7Y2mo5FGbVAiFbvoXu-c~-ooxbzhETM04ZyA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" className="w-[90px] h-[90px]" alt="" />
+                    <div className="flex items-center justify-center gap-3">
+                        <img src={logo} className="w-[50px] h-[50px]" alt="" />
                         <p className="lg:text-5xl text-3xl text-[#02AAFF] font-semibold">BlockTech</p>
                     </div>
                     <p className="text-xl text-[#A6AFBB]">Rule The World</p>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,34 +1,39 @@
-
+import logo from '../../assets/dynaqube1.png'
 const Header = () => {
-    const navItem=<>
-          <li className="lg:text-white font-normal text-lg"><a href="#home">Home</a></li>
-          <li className="lg:text-white font-normal text-lg"><a href="#feature">Features</a></li>
-          <li className="lg:text-white font-normal text-lg"><a href="#about">About</a></li>
-          <li className="lg:text-white font-normal text-lg"><a href="#products">Products</a></li>
-          <li className="lg:text-white font-normal text-lg"><a href="#tokenomics">Tokenomics</a></li>
-          <li className="text-white font-normal text-lg"><a>Staking</a></li>
-    </>
-    return (
-        <div className="navbar ">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-           {navItem}
-            </ul>
-          </div>
-          <a className="btn btn-ghost normal-case lg:text-3xl text-xl text-[#02AAFF]">BlockTech</a>
-        </div>
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-           {navItem}
+  const navItem = <>
+    <li className="lg:text-white font-normal text-lg"><a href="#home">Home</a></li>
+    <li className="lg:text-white font-normal text-lg"><a href="#feature">Features</a></li>
+    <li className="lg:text-white font-normal text-lg"><a href="#about">About</a></li>
+    <li className="lg:text-white font-normal text-lg"><a href="#products">Products</a></li>
+    <li className="lg:text-white font-normal text-lg"><a href="#tokenomics">Tokenomics</a></li>
+    <li className="text-white font-normal text-lg"><a>Staking</a></li>
+  </>
+  return (
+    <div className="navbar ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            {navItem}
           </ul>
         </div>
-     
+        <div className="flex justify-center items-center ml-5 lg:ml-0">
+          <img src={logo} className='w-[50px] h-[50px] ' alt="" />
+          <a className="btn btn-ghost normal-case lg:text-3xl text-xl text-[#02AAFF]">BlockTech</a>
+         
+        </div>
+
       </div>
-    );
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navItem}
+        </ul>
+      </div>
+
+    </div>
+  );
 };
 
 export default Header;
